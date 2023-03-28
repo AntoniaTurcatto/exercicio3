bc656a735d278694b47579fb9763c3c0336ace08
Antônia Turcatto <lucasturcatto.va048@academico.ifsul.edu.br>
2023-03-28 20:41:32 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    //A)
+    var numeroDecimal  : Float// para número decimal deve-se utilizar float ou double
+    //B)
+    var texto : String //para armazenar texto, usa-se String
+    var numeroInteiro : Int//para número inteiros, usa-se Int
+    //C)
+    var numeroInteiro2 : Int
+    var numeroDecimal2 : Float
+
+}
